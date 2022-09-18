ba76af5c57d1908129334616a22337020f2a2fe0
roadhog <xiaxiansheng000@163.com>
2022-09-18 23:35:36 +0800

    [feature] rabbitmq相关

--- spring-boot-mq-producer/src/test/java/com/roadhog/SpringBootProducerApplicationTests.java
+++ spring-boot-mq-producer/src/test/java/com/roadhog/SpringBootProducerApplicationTests.java
@@ -2,6 +2,7 @@ package com.roadhog;
 
 import com.roadhog.config.RabbitConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +15,16 @@ class SpringBootProducerApplicationTests {
     @Test
     void contextLoads() {
         rabbitTemplate.convertAndSend(RabbitConfig.EXCHANGE_NAME,"boot.123","hello world!");
+    }
+
+    @Test
+    void confirmTest(){
+        rabbitTemplate.setConfirmCallback(new RabbitTemplate.ConfirmCallback() {
+            @Override
+            public void confirm(CorrelationData correlationData, boolean b, String s) {
+                System.out.println("confirm 方法被执行了。。。");
+            }
+        });
     }
 
 }
